545094e80e1791a1e74a9ad165dbc7f8db8617d0
kkt9102 <kkt9102@gmail.com>
2022-01-21 02:04:09 +0900

    TodoTemplate 작성

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,1 +1,15 @@
 // 앱 타이틀(일정관리) children으로 내부 JSX를 props로 받아와 랜더링
+
+import React from "react";
+import './Todo.css';
+
+const TodoTemplate = ({ children }) =>{
+    return (
+        <div className="TodoTemplat">
+            <div className="app_title">일정관리</div>
+            <div className="content">{children}</div>
+        </div>
+    );
+};
+
+export default TodoTemplate;
