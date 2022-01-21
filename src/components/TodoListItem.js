0c12a235f982c5cc0a73b963cd4235e1db40ccd6
kkt9102 <kkt9102@gmail.com>
2022-01-22 01:11:57 +0900

    스타일 수정

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,7 +1,7 @@
 // 각 항목들에 대한 정보를 보여주는 컴포넌트. todo 객체를 props로 받아와 상태에 따라 다른 스타일의 UI를 보여줌
 
 import React from "react";
-import {MdCheckCircleOutline
+import {MdRemoveCircleOutline
     , MdCheckBoxOutlineBlank
     , MdCheckBox } from 'react-icons/md';
     
@@ -16,7 +16,7 @@ const TodoListItem = () => {
                 <div className="text">할 일</div>
             </div>
             <div className="remove">
-                <MdCheckCircleOutline />
+                <MdRemoveCircleOutline />
             </div>
         </div>
     );
