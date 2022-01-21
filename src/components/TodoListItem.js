9853090f5ac301934a96ad059e8e30ab1617e7f0
kkt9102 <kkt9102@gmail.com>
2022-01-21 23:43:06 +0900

    TodoListItems 작성

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,1 +1,24 @@
 // 각 항목들에 대한 정보를 보여주는 컴포넌트. todo 객체를 props로 받아와 상태에 따라 다른 스타일의 UI를 보여줌
+
+import React from "react";
+import {MdCheckCircleOutline
+    , MdCheckBoxOutlineBlank
+    , MdCheckBox } from 'react-icons/md';
+    
+import 'Todo.css';
+
+const TodoListItem = () => {
+    return(
+        <div className="TodoListItem">
+            <div className="checkbox">
+                <MdCheckBoxOutlineBlank />
+                <div className="text">할 일</div>
+            </div>
+            <div className="remove">
+                <MdCheckCircleOutline />
+            </div>
+        </div>
+    );
+};
+
+export default TodoListItem;
