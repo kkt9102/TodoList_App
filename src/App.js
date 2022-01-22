629eed6ca4c224a7c0c136b79b367deab79c2a1d
kkt9102 <kkt9102@gmail.com>
2022-01-22 14:52:29 +0900

    App.js에 할일 추가를 위한 초기작업

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useRef, useCallback } from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/ToddInsert';
@@ -22,10 +22,26 @@ const App = () => {
       checked:false,
     }
   ])
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  )
   return (
     <>
       <TodoTemplate>
-        <TodoInsert />
+        <TodoInsert onInsert={onInsert}/>
         <TodoList todos={todos} />
       </TodoTemplate>
     </>
