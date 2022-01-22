b9bdb8231865bf4dc998b27e5ac4bba51381fbec
kkt9102 <kkt9102@gmail.com>
2022-01-22 14:48:47 +0900

    input 오류수정

--- src/components/ToddInsert.js
+++ src/components/ToddInsert.js
@@ -1,12 +1,21 @@
 // 새로운 일정을 추가하는 컴포넌트. state를 통해 인풋상태관리
 
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { IoAdd } from 'react-icons/io5';
 
 const TodoInsert = () => {
+    const [ value, setVlaue ] = useState('');
+
+    const onChange = useCallback(e => {
+        setVlaue(e.target.value);
+    }, []);
+
     return(
         <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요"/>
+            <input 
+            placeholder="할 일을 입력하세요"
+            value={value}
+            onChange={onChange}/>
             <button type="submit">
                 <IoAdd />
             </button>
