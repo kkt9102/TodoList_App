70aa617e235b6dc0deeb188deecd8853b482feb8
kkt9102 <kkt9102@gmail.com>
2022-02-26 16:00:27 +0900

    할 일 메모 기능 구현 끝

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -8,16 +8,16 @@ import cn from 'classnames';
 import './Todo.css';
 
 // import './TodoListItem.scss';
-const TodoListItem = ({ todo, onRemove }) => {
-    const { text,checked } = todo;
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+    const { id,text,checked } = todo;
     return(
         <div className="TodoListItem">
-            <div className={cn(`checkbox`, { checked })}>
+            <div className={cn(`checkbox`, { checked })} onClick={() => onToggle(id)}>
                 {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
                 {/* <MdCheckBoxOutlineBlank /> */}
                 <div className="text">{text}</div>
             </div>
-            <div className="remove">
+            <div className="remove" onClick={() => onRemove(id)}>
                 <MdRemoveCircleOutline />
             </div>
         </div>
